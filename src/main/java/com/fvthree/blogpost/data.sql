8f0c1c0c6f6fdfa9538474aebebd307d0641ae10
fvthree <kadjaski@gmail.com>
2023-03-30 02:32:45 +0800

    remove

--- src/main/java/com/fvthree/blogpost/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO roles (role_id, name, created_at, updated_at) VALUES (1, 'ROLE_ADMIN', NOW(), NOW());
-INSERT IGNORE INTO roles (role_id, name, created_at, updated_at) VALUES (2, 'ROLE_USER', NOW(), NOW());
